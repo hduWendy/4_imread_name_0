9809f9b650779ff4cfd397984046d063d371e614
hduWendy <576939544@qq.com>
2020-02-28 14:49:11 +0800

    初步完成1

--- 4_imread_name_0/4_imread_name_0.cpp
+++ 4_imread_name_0/4_imread_name_0.cpp
@@ -7,7 +7,7 @@
 using namespace cv;
 int main()
 {
-	//读取本地的一张图片处理imread("1.jpg",0)，并显示出来
+	//读取本地的一张图片处理imread("1.jpg",0)，并显示出来!
 	//imread后面的文件路径可以根据自己的实际路径修改。
 	Mat img = imread("C:/Users/lenovo/Desktop/推文/1.jpg",0);
 	imshow("test", img);
