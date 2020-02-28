e531aba90ae21663fffc8672099a9f6b7f1a17b6
hduWendy <576939544@qq.com>
2020-02-28 14:46:41 +0800

    初步完成

--- 4_imread_name_0/4_imread_name_0.cpp
+++ 4_imread_name_0/4_imread_name_0.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
-
-
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
 int main()
 {
-    return 0;
+	//读取本地的一张图片处理imread("1.jpg",0)，并显示出来
+	//imread后面的文件路径可以根据自己的实际路径修改。
+	Mat img = imread("C:/Users/lenovo/Desktop/推文/1.jpg",0);
+	imshow("test", img);
+	//等待用户按键
+	waitKey(0);
+	return 0;
 }
+
 
